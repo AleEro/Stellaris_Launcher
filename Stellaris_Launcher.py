a8a1d650abd0a933b2fc277740d66270f9d128c1
yohoko <paradox.wolf@mail.ru>
2019-11-10 20:18:31 +0300

    first comma

--- Stellaris_Launcher.py
+++ Stellaris_Launcher.py
@@ -1,20 +1,42 @@
-from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5 import QtWidgets
+from PyQt5 import QtCore
+from PyQt5 import QtGui
 import sys
 
 
 class AppMainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # elements
 
+        self.table = QtWidgets.QTableWidget()
+        abst = QtWidgets.QAbstractItemView
+        self.table.setSelectionMode(abst.SingleSelection)
+        self.table.setSelectionBehavior(abst.SelectRows)
+        self.table.setDragDropMode(abst.InternalMove)
+        self.table.setDragEnabled(True)
+        self.table.setColumnCount(4)
+        self.table.setRowCount(4)
+
+        play_button = QtWidgets.QPushButton('Play')
+        play_button.setFlat(True)
+
+        accept_button = QtWidgets.QPushButton('Accept')
+        accept_button.setFlat(True)
+
+        # window proprieties
         self.setMinimumSize(QtCore.QSize(self.width(), self.height()))
         self.setWindowIcon(QtGui.QIcon('img\\ico1.png'))
         self.setWindowTitle('Stellaris Launcher 2.3 by yohko ')
+
         vbox = QtWidgets.QVBoxLayout(self)
         wdg = QtWidgets.QWidget()
-        wdg.setLayout(vbox)
-        play_button = QtWidgets.QPushButton('Play')
-        play_button.setFlat(True)
+
+        vbox.addWidget(self.table)
+        vbox.addWidget(accept_button)
         vbox.addWidget(play_button)
+        wdg.setLayout(vbox)
+
         self.setCentralWidget(wdg)
 
 
