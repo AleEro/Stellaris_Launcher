280a9182854988439bdfb1f1e57497015fd9678e
yohoko <paradox.wolf@mail.ru>
2019-11-10 19:39:22 +0300

    first comma

--- Stellaris_Launcher.py
+++ Stellaris_Launcher.py
@@ -1,1 +1,27 @@
-print('hello')
+from PyQt5 import QtWidgets, QtCore, QtGui
+import sys
+
+
+class AppMainWindow(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setMinimumSize(QtCore.QSize(self.width(), self.height()))
+        self.setWindowIcon(QtGui.QIcon('img\\ico1.png'))
+        self.setWindowTitle('Stellaris Launcher 2.3 by yohko ')
+        vbox = QtWidgets.QVBoxLayout(self)
+        wdg = QtWidgets.QWidget()
+        wdg.setLayout(vbox)
+        play_button = QtWidgets.QPushButton('Play')
+        play_button.setFlat(True)
+        vbox.addWidget(play_button)
+        self.setCentralWidget(wdg)
+
+
+#  на случай если программа запускается из оболочки
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    main = AppMainWindow()
+    main.show()
+    sys.exit(app.exec_())
+
