a45040d602e5215a2ac11bd71a3c826a6f433a72
yohoko <paradox.wolf@mail.ru>
2019-11-11 20:59:40 +0300

    0.11

--- Stellaris_Launcher.py
+++ Stellaris_Launcher.py
@@ -1,8 +1,8 @@
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore
 from PyQt5 import QtGui
-import sys
-from table_class import Table
+import sys, json
+from table_class import Table, TableData
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -10,12 +10,17 @@ class MainWindow(QtWidgets.QMainWindow):
         super().__init__(*args, **kwargs)
         # elements
         self.table = Table()
+        self.table_dt = TableData()
 
         play_button = QtWidgets.QPushButton('Play')
         play_button.setFlat(True)
+        # play_button.clicked.connect()
 
-        accept_button = QtWidgets.QPushButton('Accept')
-        accept_button.setFlat(True)
+        load_btn = QtWidgets.QPushButton('Load')
+        load_btn.setFlat(True)
+        play_button.clicked.connect(self.fill_table)
+        settings = QtWidgets.QPushButton('settings')
+        settings.setFlat(True)
 
         # window proprieties
         self.setMinimumSize(QtCore.QSize(self.width(), self.height()))
@@ -26,14 +31,24 @@ class MainWindow(QtWidgets.QMainWindow):
         wdg = QtWidgets.QWidget()
 
         vbox.addWidget(self.table)
-        vbox.addWidget(accept_button)
+        vbox.addWidget(load_btn)
         vbox.addWidget(play_button)
+        vbox.addWidget(settings)
         wdg.setLayout(vbox)
 
         self.setCentralWidget(wdg)
+        self.mods_registry = self.load_fd('mods_registry.json')
 
-    def fill_table(self):
-        pass
+    def fill_table(self, a='hello'):
+        print(a)
+
+    @staticmethod
+    def load_fd(name, dir=r'Stellaris Launcher/'):
+        print('ss')
+        with open(dir+name, 'r', encoding='utf_8') as data:
+            b = json.load(data)
+            # print(b)
+        return b
 
 
 #  на случай если программа запускается из оболочки
--- table_class.py
+++ table_class.py
@@ -1,16 +1,21 @@
 from PyQt5 import QtWidgets, QtCore, QtGui
 
 
-class Table(QtWidgets.QTableWidget):
+class TableData(QtGui.QStandardItemModel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+
+class Table(QtWidgets.QTableView):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.sim = TableData(self)
+        self.setModel(self.sim)
         self.setSelectionMode(self.SingleSelection)
         self.setSelectionBehavior(self.SelectRows)
         self.setEditTriggers(self.NoEditTriggers)
         self.setDragDropMode(self.InternalMove)
         self.setDragEnabled(True)
-        self.setColumnCount(4)
-        self.setRowCount(4)
         self.setGridStyle(QtCore.Qt.NoPen)
         self.setShowGrid(False)
 
